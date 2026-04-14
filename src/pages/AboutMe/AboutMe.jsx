cdb3ce3e226ed19cfe1cf9d046ed61925ce4ea06
merikhimo <merikhimo@gmail.com>
2026-04-14 18:00:49 +0300

    add about mobile version

--- src/pages/AboutMe/AboutMe.jsx
+++ src/pages/AboutMe/AboutMe.jsx
@@ -8,13 +8,13 @@ import Carousel from '@/components/imageCarousel/imageCarousel'
 const AboutMe = (showTitle = true) => {
 
     const [item, setItem] = useState({
-        description: "Anton Shell — молодой байер из Москвы, превращающий продажи в искусство. Начиная свой путь с китайских платформ в 15 лет, он быстро понял разницу между массовым рынком и настоящим стилем. Теперь доставляет вещи из Европы, США и Великобритании, собирая гардеробы, которые говорят громче слов. Визуал — его оружие. Продуманная стилизация, сильные промо-съемки, точный вкус. За два года работы Антон успел посотрудничать с многими брендами: Jaded London, Racer Worldwide, Alice Hollywood, Nofaithstudios, Project gr, Yori Sport и другие. Антон не просто продает одежду — он продает образ жизни. sdakff adawpfdwfkwfwofkwf", 
+        description: "Anton Shell — молодой байер из Москвы, превращающий продажи в искусство. Начиная свой путь с китайских платформ в 15 лет, он быстро понял разницу между массовым рынком и настоящим стилем. Теперь доставляет вещи из Европы, США и Великобритании, собирая гардеробы, которые говорят громче слов. Визуал — его оружие. Продуманная стилизация, сильные промо-съемки, точный вкус. За два года работы Антон успел посотрудничать с многими брендами: Jaded London, Racer Worldwide, Alice Hollywood, Nofaithstudios, Project gr, Yori Sport и другие. Антон не просто продает одежду — он продает образ жизни.sdakffadawpfdwfkwfwofkwf", 
     })
 
     return (
         <>
         {showTitle ? <h1 className={styles.title}>Обо мне</h1> : ""}
-        <div className={styles.mainShow}>
+        <div className={styles.mainContainer}>
         <div className={styles.textfield}>
             <div className={styles.textfieldinner}>
                 <span>{item.description}</span>
@@ -23,7 +23,7 @@ const AboutMe = (showTitle = true) => {
             <div className={styles.textfieldinner}>
                 <Carousel images={[image1,image2]}/>
             </div>
-        <div className={styles.textfield}>
+        <div className={styles.textfield2}>
             <div className={styles.textfieldinner}>
                 <span>{item.description}</span>
             </div>
